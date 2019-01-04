00e17b87840b53b4798f6e93e250daa5356f0949
Rbrady98 <rian.brady1998@gmail.com>
2019-01-04 15:56:22 +0000

    Finish web scraping and put in method

--- BPyLTable.py
+++ BPyLTable.py
@@ -4,17 +4,27 @@
 from bs4 import BeautifulSoup
 import requests
 
-print('hi')
+def getTable():
+    # Get data from the guardians website
+    response = requests.get('https://www.theguardian.com/football/premierleague/table')
+    soup = BeautifulSoup(response.text, 'html.parser')
+    table = soup.find(class_='table__container')
+    teams = table.tbody.find_all('tr')
 
-# Get data from the guardians website
-response = requests.get('https://www.theguardian.com/football/premierleague/table')
+    tableDict = {}
+    for team in teams:
+        # Get team name
+        name = team.find(class_='table-column--main').a.string.replace('\n', '')
+        # Get other team info
+        teamInfo = (team.find_all('td'))[2:10]
+        pointsList = []
+        for score in teamInfo:
+            pointsList.append(score.string)
 
-soup = BeautifulSoup(response.text, 'html.parser')
+        tableDict[name] = pointsList
 
-table = soup.find(class_='table__container')
-teams = table.tbody.find_all('tr')
+    # Return dictionary with team names as keys and pointsList as the values
+    return tableDict
 
-for i in range(len(teams)):
-    team = teams[i]
-    name = team.find(class_='table-column--main').a.string.replace('\n', '')
-    print(str(i) + ' ' + name)
+def main():
+
