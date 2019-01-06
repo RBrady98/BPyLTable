c8665e189b87d1eae0f33d89d06a2a8e3f33fcf9
Rbrady98 <rian.brady1998@gmail.com>
2019-01-06 20:23:03 +0000

    Added error checking for position flag and replaced print with click.echo

--- BPyLTable.py
+++ BPyLTable.py
@@ -33,21 +33,21 @@ def getTable():
 def printTeamLine(name, infoList, width, centerVal):
     teamInfo = '{:<2} {}'.format(int(infoList[0]) + 1, name)
     teamPoints = '{}  {}  {}   {}   {}  {}  {}  {} '.format(*infoList[1:])
-    print('|{:<20}{:>{w}}|'.format(teamInfo, teamPoints, w=width-20).center(centerVal))
+    click.echo('|{:<20}{:>{w}}|'.format(teamInfo, teamPoints, w=width-20).center(centerVal))
 
 
 
 def printTableLine(width, centerVal):
-    print(('+' + ('-' * width) + '+').center(centerVal))
+    click.echo(('+' + ('-' * width) + '+').center(centerVal))
 
 
 
 def displayTableTop(width, column):
-    # Get width of terminal so info can be printed in the center
+    # Get width of terminal so info can be printed the center
     printTableLine(width, column)
-    print('|{:^{w}}|'.format('BARCLAYS PREMIER LEAGUE', w=width).center(column))
+    click.echo('|{:^{w}}|'.format('BARCLAYS PREMIER LEAGUE', w=width).center(column))
     printTableLine(width, column)
-    print('|{:<10}{:>{w}}|'.format('P  Team', 'GP  W   D   L   F   A   GD  Pts', w=width-10).center(column))
+    click.echo('|{:<10}{:>{w}}|'.format('P  Team', 'GP  W   D   L   F   A   GD  Pts', w=width-10).center(column))
     printTableLine(width, column)
 
 
@@ -71,12 +71,15 @@ def main(position, top6):
     leagueTable = getTable()
     
     # If no flags passed then print the full table
-    if position == -1 and top6 == False:
-        displayTable()
-    elif position != -1:
-        displayTeamAtPos(leagueTable, position - 1)
+    if position != -1:
+        if position >= 1 and position <= 20:
+            displayTeamAtPos(leagueTable, position - 1)
+        else:
+            click.echo('Error: --position value must be in the range of 1 to 20 inclusive')
     elif top6:
         displayTop6()
+    elif position == -1 and top6 == False:
+        displayTable()
 
 
 if __name__ == '__main__':
